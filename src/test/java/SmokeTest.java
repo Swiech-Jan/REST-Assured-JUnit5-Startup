de6d055b50da50fc0d0fcc42211e0f2416f68cf5
Swiech-Jan <semper666@gmail.com>
2022-12-19 21:07:08 +0100

    Added basic test

---
+++ src/test/java/SmokeTest.java
@@ -0,0 +1,25 @@
+import static io.restassured.RestAssured.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class SmokeTest {
+
+    @Test
+    void test1() {
+
+        Response response = get("https://jsonplaceholder.typicode.com/albums/1");
+
+        System.out.println("Response: : " + response.asString());
+        System.out.println("Status Code : " + response.getStatusCode());
+        System.out.println("Header : " + response.getHeader("content-type"));
+
+        int statusCode = response.getStatusCode();
+        expect().statusCode(200);
+
+    }
+}
